8bc86ca3664682708235ec6fdf14374dd5a578df
John Lyndo Anuada <157618361+anuadajohnlyndo@users.noreply.github.com>
2024-04-21 23:38:57 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+/**function chckCookies(){
+    var username = "Lyndo";
+    var loadhtml = "";
+    if(username == "Lyndo"){
+        loadhtml = "index.html";
+    }
+    else{
+        loadhtml = "homepage.html";
+    }
+    location.href = loadhtml;
+}
+
+
+document.getElementById("btnSignin").addEventListener('click', function(){
+    
+})**//
